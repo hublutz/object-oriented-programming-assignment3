97c91ffdb2aa9b9e4096a6b5eb21c727428aa3b4
asher-stav <asherst@post.bgu.ac.il>
2023-06-09 08:51:05 +0300

    Add Point class, fields and methods

---
+++ hw3/src/BusinessLayer/Tiles/Point.java
@@ -0,0 +1,62 @@
+package BusinessLayer.Tiles;
+
+/**
+ * Class Point represents the placement of a Tile in the board
+ */
+public class Point
+{
+    private int x;
+    private int y;
+
+    /**
+     * Point constructor
+     * @param x the x-axis value
+     * @param y the y-axis value
+     */
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * X-axis value getter
+     * @return the x-axis value of the point
+     */
+    public int getX()
+    {
+        return x;
+    }
+
+    /**
+     * Y-axis value getter
+     * @return the y-axis value of the point
+     */
+    public int getY()
+    {
+        return y;
+    }
+
+    /**
+     * Changes the axes values of the point to the given ones
+     * @param x the nes x-axis value
+     * @param y the new y-axis value
+     */
+    public void move(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Calculates the Euclidean range between two points
+     * @param other the point to calculate the distance from
+     * @return the distance between this point and the other one
+     */
+    public double range(Point other)
+    {
+        int xDelta = this.x - other.x;
+        int yDelta = this.y - other.y;
+        return Math.sqrt((xDelta * xDelta) + (yDelta * yDelta));
+    }
+}
