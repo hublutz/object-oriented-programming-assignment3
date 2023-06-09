10712d4cf3462c250be038b7dded3ec9ab2daf16
asher-stav <asherst@post.bgu.ac.il>
2023-06-09 09:37:24 +0300

    Add EmptyTile class, fields and methods

---
+++ hw3/src/BusinessLayer/Tiles/EmptyTile.java
@@ -0,0 +1,27 @@
+package BusinessLayer.Tiles;
+
+/**
+ * Class EmptyTile represents an empty space in the board
+ */
+public class EmptyTile extends Tile
+{
+    public static final char EMPTY_TILE_CHAR = '.';
+
+    /**
+     * EmptyTile constructor, initialises the tile char to be
+     * '.'
+     * @param x the x-axis value of the position
+     * @param y the y-axis value of the position
+     */
+    public EmptyTile(int x, int y)
+    {
+        super(EMPTY_TILE_CHAR, x, y);
+    }
+
+    /**
+     * Updates the EmptyTile after a game tick.
+     * Practically, it is empty since the tile can't update
+     */
+    @Override
+    public void onGameTick() { }
+}
