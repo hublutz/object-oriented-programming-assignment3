d79fba744111dfdf04a653d8805ca339b57a4ba2
asher-stav <asherst@post.bgu.ac.il>
2023-06-16 20:01:05 +0300

    Add Enemy death callback interface and integrate it in Enemy

--- hw3/src/BusinessLayer/Tiles/EnemyTiles/Enemy.java
+++ hw3/src/BusinessLayer/Tiles/EnemyTiles/Enemy.java
@@ -10,20 +10,28 @@ public abstract class Enemy extends UnitTile
 {
     private int experienceValue;
     private Player player;
-
+    private IEnemyDeathCallback deathCallback;
     /**
      * Enemy constructor, receives all UnitTile parameters and
      * experience value and the player reference
      * @param experienceValue Experience value the player receives upon killing the enemy
      * @param player The player tile reference
+     * @param deathCallback Defines the behaviour of the enemy upon death
      */
     public Enemy(char tile, int x, int y, String name, int healthPool,
                  int attackPoints, int defencePoints, IMessageCallback messageCallback,
-                 int experienceValue, Player player)
+                 int experienceValue, Player player, IEnemyDeathCallback deathCallback)
     {
         super(tile, x, y, name, healthPool, attackPoints,
                 defencePoints, messageCallback);
         this.experienceValue = experienceValue;
         this.player = player;
+        this.deathCallback = deathCallback;
+    }
+
+    @Override
+    public void onDeath()
+    {
+        this.deathCallback.callEnemyDeath(this);
     }
 }
