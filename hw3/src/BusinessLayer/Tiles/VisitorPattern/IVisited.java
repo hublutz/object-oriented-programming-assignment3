4418639cb49522e2cf17a2e52a20cdcda299b220
hublutz <livshitz@post.bgu.ac.il>
2023-06-21 20:04:55 +0300

    added visitor and visited files

--- hw3/src/BusinessLayer/Tiles/VisitorPattern/IVisited.java
+++ hw3/src/BusinessLayer/Tiles/VisitorPattern/IVisited.java
@@ -1,4 +1,8 @@
 package BusinessLayer.Tiles.VisitorPattern;
 
 public interface IVisited {
+    /**
+     * accept function as part of the visitor pattern
+     * */
+    public void accept(IVisitor visitor);
 }
