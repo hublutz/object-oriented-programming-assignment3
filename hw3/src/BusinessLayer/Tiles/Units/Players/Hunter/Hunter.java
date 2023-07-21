9b0842d532736f86a22c06762ed40bad7bd26982
asher-stav <asherst@post.bgu.ac.il>
2023-07-21 19:22:53 +0300

    Implement Hunter class methods: level up, ability cast, game tick and description

--- hw3/src/BusinessLayer/Tiles/Units/Players/Hunter/Hunter.java
+++ hw3/src/BusinessLayer/Tiles/Units/Players/Hunter/Hunter.java
@@ -5,17 +5,19 @@ import BusinessLayer.Tiles.Units.EnemyTiles.Enemy;
 import BusinessLayer.Tiles.Units.Players.Player;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class Hunter represents a Hunter player type
  */
 public class Hunter extends Player
 {
-    private int range;
+    private final int range;
     private int arrowsCount;
     private int ticksCount;
 
     private static final int INITIAL_TICKS_COUNT = 0;
+    private static final int NO_ARROWS = 0;
     private static final int ARROWS_COUNT_INCREASE = 10;
     private static final int ATTACK_POINTS_LEVEL_UP_INCREASE = 2;
     private static final int DEFENCE_POINTS_LEVEL_UP_INCREASE = 1;
@@ -59,9 +61,95 @@ public class Hunter extends Player
         this.ticksCount = INITIAL_TICKS_COUNT;
     }
 
+    /**
+     * This method performs the level up procedure of the Hunter player class
+     */
+    @Override
+    public void levelUp()
+    {
+        super.levelUp();
+        this.arrowsCount += this.playerLevel * ARROWS_COUNT_INCREASE;
+        this.attackPoints += this.playerLevel * ATTACK_POINTS_LEVEL_UP_INCREASE;
+        this.defencePoints += this.playerLevel * DEFENCE_POINTS_LEVEL_UP_INCREASE;
+    }
+
+    /**
+     * This method performs the game tick procedure of the Hunter player class
+     */
+    @Override
+    public void onGameTick()
+    {
+        super.onGameTick();
+        if (this.ticksCount == ARROWS_COUNT_INCREASE)
+        {
+            this.arrowsCount += this.playerLevel;
+            this.ticksCount = INITIAL_TICKS_COUNT;
+        }
+        else
+        {
+            this.ticksCount++;
+        }
+    }
+
+    /**
+     * This method casts the Shoot ability of the Hunter
+     * @param enemies the enemies list of the game board
+     */
     @Override
     public void castAbility(List<Enemy> enemies)
     {
+        if (this.arrowsCount == NO_ARROWS)
+        {
+            List<Enemy> enemiesByRange = enemies.stream().sorted((enemy1, enemy2) ->
+                            Double.compare(this.range(enemy1), this.range(enemy2))).
+                            collect(Collectors.toList());
+            if (!enemiesByRange.isEmpty() && enemiesByRange.get(0).range(this) <= this.range)
+            {
+                this.arrowsCount--;
+                Enemy closestEnemy = enemiesByRange.get(0);
 
+                this.messageCallback.passMessage("Hunter Shoot!");
+                this.messageCallback.passMessage(this.description());
+                this.messageCallback.passMessage(closestEnemy.description());
+
+                closestEnemy.defend(this.attackPoints);
+                this.checkIfEnemyIsDeadAndGetEx(closestEnemy);
+                if (closestEnemy.isDead())
+                {
+                    closestEnemy.onDeath();
+                }
+            }
+            else
+            {
+                this.messageCallback.passMessage("Can't cast Hunter ability! No enemies in range");
+            }
+        }
+        else
+        {
+            this.messageCallback.passMessage("Can't cast Hunter ability! not enough arrows");
+        }
+    }
+
+    /**
+     * This method provides a description and statistics for the Hunter
+     * @return a String containing the Hunter description
+     */
+    @Override
+    public String description()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append(super.description());
+
+        builder.append("\t- Range: ");
+        builder.append(this.range);
+        builder.append('\n');
+        builder.append("\t- Arrows Count: ");
+        builder.append(this.arrowsCount);
+        builder.append('\n');
+        builder.append("\t- Ticks Count: ");
+        builder.append(this.ticksCount);
+        builder.append('\n');
+
+        return builder.toString();
     }
 }
